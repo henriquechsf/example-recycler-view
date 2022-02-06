4acf63164456b1a70284533ee6e2172166562137
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-02-06 10:59:07 -0300

    simple list adapter

--- app/src/main/java/tech/henriquedev/unidade03/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/unidade03/MainActivity.kt
@@ -2,10 +2,32 @@ package tech.henriquedev.unidade03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
+/**
+ * List + Adapter
+ * para renderizar um lista na tela e necessario
+ * 1. Lista xml > 2. dados tipo List > 3. Adapter
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // lista
+        val listView = findViewById<ListView>(R.id.list_view)
+
+        // dados
+        val data = listOf<String>("Umuarama", "Maringa", "Cascavel", "Curitiba")
+
+        // adapter
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            data
+        )
+
+        listView.adapter = adapter
     }
 }
